28a53d3cf052c544689f4bc17aef52aabc24e4be
Linh Doan <thuylinhdoan1999@gmail.com>
2021-05-06 22:01:06 +0700

    Format code reservation controller

--- src/main/java/com/example/demo/utils/ResponseUtils.java
+++ src/main/java/com/example/demo/utils/ResponseUtils.java
@@ -1,9 +1,25 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// © 2021 IDTU-CS3332IRFA-21TSP
+//
+/////////////////////////////////////////////////////////////////////////////
+
 package com.example.demo.utils;
 
 import org.springframework.http.HttpStatus;
 
 import com.example.demo.bean.ResultBean;
 
+/**
+ * [OVERVIEW] Response Utils.
+ *
+ * @author: LinhDT
+ * @version: 1.1
+ * @History
+ * [NUMBER]  [VER]     [DATE]          [USER]             [CONTENT]
+ * --------------------------------------------------------------------------
+ * 001       1.0       2021/04/20      LinhDT             Create new
+*/
 public class ResponseUtils {
 
     public static HttpStatus getResponseStatus(ResultBean result) {
